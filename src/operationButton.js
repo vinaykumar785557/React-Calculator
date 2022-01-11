0864053a0e8d77a808a435934397cca1dd617d37
vinaykumar785557 <c0785557@mylambton.ca>
2022-01-11 14:32:21 -0500

    calculator using useReducer

---
+++ src/operationButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ACTIONS } from "./App";
+function operationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
+
+export default operationButton;
